80d1e5af4e58b6aa1c65d142319f0bfc865c87f6
vipulgis <vipul.pec@gmail.com>
2015-06-24 18:17:08 +0300

    Testing Commit

--- JAVA_Samples/src/Samples/Basics/FactorialExample.java
+++ JAVA_Samples/src/Samples/Basics/FactorialExample.java
@@ -7,7 +7,7 @@ public class FactorialExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FactorialExample F1 = new FactorialExample();
-        BigInteger NasBigInteger = BigInteger.valueOf(10);
+        BigInteger NasBigInteger = BigInteger.valueOf(5);
 
 		System.out.println(F1.factorial(NasBigInteger));
 		
